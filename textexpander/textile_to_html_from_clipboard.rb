a46b4aaaaad804c9873b49a7f7e15cc0942fd8a3
Matthew Fallshaw <matthew.fallshaw@gmail.com>
2009-03-31 11:24:36 +1100

    cleanup: move primarily TextExpander driven app into relevant dir

--- textexpander/textile_to_html_from_clipboard.rb
+++ textexpander/textile_to_html_from_clipboard.rb
@@ -2,8 +2,9 @@
 
 # Convert the contents of the clipboard to html (with
 # RedCloth[http://whytheluckystiff.net/ruby/redcloth/], so assuming it's
-# Textile or Markdown) and send it to STDOUT (I use this from an AppleScript
-# TextExpander[http://smileonmymac.com/TextExpander/] snippet).
+# Textile or Markdown) and send it to STDOUT
+# (I use this from a shell TextExpander[http://smileonmymac.com/TextExpander/]
+# snippet).
 
 %w[rubygems redcloth].each {|l| require l }
 
