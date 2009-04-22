a04bee39b76e50d70ac8d155def1408ce337f054
Matthew Fallshaw <matthew.fallshaw@gmail.com>
2009-04-22 17:20:04 +1000

    minor: cleanup

--- textexpander/bitly.rb
+++ textexpander/bitly.rb
@@ -6,10 +6,9 @@ clip_url = `pbpaste`  # OSX dependency
 unless clip_url.match(/^http/)
   puts clip_url
 else
-  require 'uri'
+  %w[uri rubygems json].each {|l| require l }
   encoded_url = URI.encode(clip_url)
   curl_url_json = `curl --stderr /dev/null \"http://api.bit.ly/shorten?longUrl=#{ encoded_url }&version=2.0.1&login=yourbitlylogin&apiKey=yourbitlyapikey&history=1\"`
-  %w[rubygems json].each {|l| require l }
   bitly_result = JSON.parse(curl_url_json)
   print bitly_result["results"].values.first["shortUrl"]
 end
