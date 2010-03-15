c7ea78c8d1c9689e82ef8a30e866e6fc09cac362
Matthew Fallshaw <matthew.fallshaw@gmail.com>
2010-03-15 11:56:42 +1100

    don't whine on weekends

--- cron/backup-google-cals-check.rb
+++ cron/backup-google-cals-check.rb
@@ -1,9 +1,16 @@
 #!/usr/bin/env ruby
 Dir.chdir("/Users/matt/Dropbox/Archive")
 
+def weekday?
+  not %w[Sat Sun].include?(Time.now.strftime("%a"))
+end
+def old?(file)
+  (Time.now - File.mtime(file)) > (2 * 24 * 60 * 60)
+end
+
 `date > ics-checked`
 %w[matthewfallshaw investling trikeit trikeitshared trikemtgrm].collect {|f| "#{f}.ics"}.each do |file|
-  if (Time.now - File.mtime(file)) > (2 * 24 * 60 * 60)
+  if weekday? && old?(file)
     require 'rubygems'; require 'action_view'; include ActionView::Helpers::DateHelper
     abort "Problem: the iCal backup script hasn't updated #{file} for #{distance_of_time_in_words_to_now(File.mtime(file))}"
   end
--- cron/backup-google-cals.rb
+++ cron/backup-google-cals.rb
@@ -17,9 +17,17 @@ end
 threads.each { |t| t.join }
 
 # Check that the check is happening
+
+def weekday?
+  %w[Sat Sun].include?(Time.now.strftime("%a"))
+end
+def old?(file)
+  (Time.now - file.mtime) > (2 * 24 * 60 * 60)
+end
+
 require 'pathname'
 checkfile = Pathname.new("ics-checked")
-if !checkfile.exist? || ((Time.now - checkfile.mtime) > (2 * 24 * 60 * 60))
+if not(checkfile.exist?) || (weekday? && old?(checkfile))
   require 'rubygems'; require 'action_view'; include ActionView::Helpers::DateHelper
   if checkfile.exist?
     warn "Problem: iCal backup script ran okay, but the script that checks that this script is running hasn't run for #{distance_of_time_in_words_to_now(checkfile.mtime)}"
