0375358b1df0d43499abd8b384a313923dff05b7
Matthew Fallshaw <matthew.fallshaw@gmail.com>
2010-05-18 11:19:44 +1000

    bugfix: wrong path

--- Scripts/SetTimeMachinedrive.sh
+++ Scripts/SetTimeMachinedrive.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-[ -d /Volumes/Time\ Machine/ ] || [ -d /Volumes/Spare/ ] && osascript /Users/matt/dev/utils/Scripts/Set\ Time\ Machine\ drive.scpt
+exit 0
+
+[ -d /Volumes/Time\ Machine/ ] || [ -d /Volumes/Spare/ ] && osascript /Users/matt/dev/utils/Scripts/SetTimeMachinedrive.scpt
