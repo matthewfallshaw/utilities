2503e1a7703aa191878c5a29ab34d57f3359a4c4
Matthew Fallshaw <matthew.fallshaw@gmail.com>
2012-10-28 08:30:46 +1100

    hunt for bells to play, and play them at a reasonable volume

--- bells.rb
+++ bells.rb
@@ -1,11 +1,26 @@
 #!/usr/bin/env ruby
 
-threads = []
-Dir["*.mp3"].each do |sound|
-  threads << Thread.new(sound) do |asound|
-    sleep(rand * 2)
-    system("afplay '#{asound}'")
+def bells
+  %w[*.mp3 ~/Sounds/*Bell*.mp3].each do |g|
+    if not(Dir[File.expand_path(g)].empty?)
+      return Dir[File.expand_path(g)]
+    end
   end
+  raise "Can't find any bells to ring."
 end
 
-threads.each {|t| t.join }
+current_vol = `osascript -e 'get output volume of (get volume settings)'`.chomp.to_i
+begin
+  system('osascript -e "set volume output volume 20"')
+  threads = []
+  bells.each do |sound|
+    threads << Thread.new(sound) do |asound|
+      sleep(rand * 2)
+      system("afplay '#{asound}'")
+    end
+  end
+
+  threads.each {|t| t.join }
+ensure
+  system("osascript -e 'set volume output volume #{current_vol}'")
+end
