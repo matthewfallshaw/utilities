195110ba513f496fc8d2d05115b1ade33d389a0a
Matthew Fallshaw <matthew.fallshaw@gmail.com>
2012-11-11 05:01:35 +1100

    cleanup including emailing report as default action

--- diskmonitor/diskmonitor.rb
+++ diskmonitor/diskmonitor.rb
@@ -1,30 +1,26 @@
 #!/usr/bin/env ruby
-begin
-  require_relative 'lib/diskmonitor'
-rescue
-  file = __FILE__
-  file = File.expand_path(File.readlink(file), File.dirname(file)) while File.symlink?(file)
-  require File.join(File.dirname(file), "lib", "diskmonitor")
-end
+
+file = __FILE__
+file = File.expand_path(File.readlink(file), File.dirname(file)) while File.symlink?(file)
+require File.join(File.dirname(file), "lib", "diskmonitor")
 
 require 'optparse'
 
 if File.basename($0).match(/^reportproblematicdrives/)
-  action = :mail_problematic_drives
-else
-  action = nil
+  action = :email
+else  # default action
+  action = :email
 end
 
 opts = OptionParser.new do |opts|
 
-  opts.banner = "Usage: #$0 [OPTION]... report"
-  opts.separator "Monitor and report disk problems before they occur."
-  opts.separator ""
-  opts.separator "Specific options:"
-  opts.on("-e", "--email", String, "Mail report to who (default to the user running the command).") do |e|
+  opts.banner = "Report on disk problems."
+  opts.separator "Usage: #$0 [options...] [ACTION]"
+  opts.separator "  ACTION: currently only 'email' (default: email)"
+  opts.on("-e", "--email", String, "Mail report here, rather than the user running the command.") do |e|
     DiskMonitor.email_address = e
   end
-  opts.on_tail("-h", "--help", "Show this message") do
+  opts.on("-h", "--help", "Show this message") do
     puts opts
     exit
   end
@@ -32,31 +28,30 @@ opts = OptionParser.new do |opts|
   begin
     opts.parse!(ARGV)
 
-    ARGV.each do |action|
-      case action
-      when "report"
-        action = :mail_problematic_drives
+    ARGV.each do |arg|
+      case arg
+      when "email", "report"  # 'report' for backwards compatibility
+        action = :email
       else
-        STDERR.puts "Unknown action: #{action}"
-        STDERR.puts
+        STDERR.puts "#$0: unknown ACTION -- #{arg}"
         STDERR.puts opts
-        exit 1
+        exit false
       end
     end
 
     case action
-    when :mail_problematic_drives
+    when :email
       puts "Checking drives..."
       DiskMonitor.mail_problematic_drives
     else
-      puts opts
-      exit 0
+      STDERR.puts "#$0: missing ACTION"
+      STDERR.puts opts
+      exit false
     end
 
   rescue OptionParser::InvalidOption => e
     $stderr.puts e.message
-    $stderr.puts
     $stderr.puts opts
-    exit
+    exit false
   end
 end
