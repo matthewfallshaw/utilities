30f39da2a7a034e1450805a34e0eb23ff95fe285
Matthew Fallshaw <m@fallshaw.me>
2017-04-23 09:27:34 +1000

    don't spam stdout on clear, generate room scripts (& cleanup)

--- Scripts/Wemo/install.rb
+++ Scripts/Wemo/install.rb
@@ -1,9 +1,21 @@
 #!/usr/bin/env ruby
 
-# Dir.cd(File.dirname($0))
-# Dir.  … remove all files other than this one
+ROOMS = ["Office", "Alex"]
 
-switches = `wemo list`.split(/\n/).collect {|s| s[/Switch: (.*)/,1]}
+# Remove old scripts
+Dir["Wemo*{On,Off}"].each {|f| File.delete(f) }
+
+# Add any new switches to switches.txt
+#  (we do this because switches sometimes temporarily fall off the network;
+#   manually edit switches.txt to remove a switch you no longer have)
+currently_detectable_switches = `wemo list`.split(/\n/).collect {|s| s[/Switch: (.*)/,1]}
+File.open("switches.txt", "a") { |f| currently_detectable_switches.each { |switch| f.puts switch } }
+system("sort -u switches.txt -o switches.txt")
+
+# Switches
+switches = File.readlines("switches.txt").collect(&:chomp)
+
+# Create switch scripts
 switches.each do |switch|
   ["On", "Off"].each do |state|
     File.open("Wemo#{switch.gsub(/ /,'')}#{state}", "w") do |f|
@@ -12,9 +24,24 @@ switches.each do |switch|
 #!/bin/bash
 WEMOSWITCH="#{switch}"
 SWITCHSTATE=#{state}
-/usr/local/bin/wemo switch "$WEMOSWITCH" $SWITCHSTATE || /usr/local/bin/wemo clear && /usr/local/bin/wemo switch "$WEMOSWITCH" $SWITCHSTATE
+/usr/local/bin/wemo switch "$WEMOSWITCH" $SWITCHSTATE || /usr/local/bin/wemo clear > /dev/null && /usr/local/bin/wemo switch "$WEMOSWITCH" $SWITCHSTATE
 END
-      f.chmod(0777)
     end
   end
 end
+
+# Create room scripts
+ROOMS.each do |room|
+  ["On", "Off"].each do |state|
+    File.open("Wemo#{room}#{state}", "w") do |f|
+      f.puts "#!/bin/sh"
+      switches.select {|s| s[/^#{room}/] }.each do |switch|
+        f.puts "/usr/local/bin/wemo switch \"#{switch}\" #{state} &> /dev/null"
+      end
+      STDOUT.puts f.path
+    end
+  end
+end
+
+# Make scripts executable
+Dir["Wemo*{On,Off}"].each {|f| File.chmod(0775, f) } # user, group: rwx, other: rx
